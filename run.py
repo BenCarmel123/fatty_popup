c248bfa1ae0820a131f014eb65a03c4984f64a9b
Ben Carmel <Bencarmel1@mail.tau.ac.il>
2025-01-24 00:51:23 +0200

    try11

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+Untitled
+
 from flask import Flask
 from db import db
 from routes.main import main
@@ -7,21 +9,27 @@ import os
 app = Flask(__name__)
 base_dir = os.path.abspath(os.path.dirname(__file__))
 db_dir = os.path.join(base_dir, 'tables')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'tables', 'main.db')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(base_dir, 'tables', 'main.db')
 app.config['SQLALCHEMY_BINDS'] = {
-   'event_db': 'sqlite:////' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'tables', 'event.db')
+    'event_db': 'sqlite:////' + os.path.join(base_dir, 'tables', 'event.db')
 }
+
 app.config.update(
-  ADMIN_USERNAME='affogato_master',
-  ADMIN_PASSWORD='BenjiBear1',
-  SQLALCHEMY_TRACK_MODIFICATIONS=False,
-  SECRET_KEY=os.getenv('SECRET_KEY', 'benji')
+   ADMIN_USERNAME='affogato_master',
+   ADMIN_PASSWORD='BenjiBear1',
+   SQLALCHEMY_TRACK_MODIFICATIONS=False,
+   SECRET_KEY=os.getenv('SECRET_KEY', 'benji')
 )
-os.makedirs('/home/BenCarmel123/fatty-popup/tables', exist_ok=True)
+
+os.makedirs(os.path.join(base_dir, 'tables'), exist_ok=True)
+
 db.init_app(app)
 with app.app_context():
-   db.create_all()
+    db.create_all()
+
 app.register_blueprint(main)
 app.register_blueprint(api, url_prefix='/api')
+
 if __name__ == '__main__':
-   app.run(debug=True)
+    app.run(debug=True)
