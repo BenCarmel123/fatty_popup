a5b597e42e17b197a4184be1892d503935ce2675
Ben Carmel <Bencarmel1@mail.tau.ac.il>
2025-01-24 00:24:14 +0200

    your try8

--- run.py
+++ run.py
@@ -2,18 +2,25 @@ from flask import Flask
 from db import db
 from routes.main import main
 from routes.api import api
+import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/BenCarmel123/fatty-popup/tables/main.db'
+base_dir = os.path.abspath(os.path.dirname(__file__))
+db_dir = os.path.join(base_dir, 'tables')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'tables', 'main.db')
 app.config['SQLALCHEMY_BINDS'] = {
-    'event_db': 'sqlite:////home/BenCarmel123/fatty-popup/tables/event.db'
+    'event_db': 'sqlite:////' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'tables', 'event.db')
 }
 
-app.config['ADMIN_USERNAME'] = 'affogato_master'
-app.config['ADMIN_PASSWORD'] = 'BenjiBear1'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'benji'
+app.config.update(
+    ADMIN_USERNAME=os.getenv('ADMIN_USERNAME', 'default_user'),
+    ADMIN_PASSWORD=os.getenv('ADMIN_PASSWORD', 'default_pass'),
+    SQLALCHEMY_TRACK_MODIFICATIONS=False,
+    SECRET_KEY=os.getenv('SECRET_KEY', 'default_secret')
+)
+
+os.makedirs('/home/BenCarmel123/fatty-popup/tables', exist_ok=True)
 
 db.init_app(app)
 with app.app_context():
