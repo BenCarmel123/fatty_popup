f5e9c94c48bf7863bb317b0a6e1c86c99f81e1af
Ben Carmel <Bencarmel1@mail.tau.ac.il>
2025-01-24 09:53:22 +0200

    try14

--- form_validations/event_valid.py
+++ form_validations/event_valid.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template, request
 from datetime import datetime, timedelta
 
+def valid_instagram(instagram):
+    errors = []
+    if instagram[0] != "@":
+        errors.append("Instagram must start with '@'")
+    if len(instagram) < 2 or len(instagram) > 30:
+        errors.append("Instagram must be between 2 and 30 characters")
+    return errors
+
 def valid_phone(phone):
     errors = []
     if phone[:2]!="05":
@@ -13,6 +21,26 @@ def valid_phone(phone):
             errors.append("Phone number must have 10 digits")
     return errors
 
+def valid_email(email):
+    errors = []
+    if "@" not in email or "." not in email:
+        errors.append("Email must contain '@' and '.'")
+    if email.count("@") > 1 or email.count(".") > 1:
+        errors.append("Email must contain only one '@' and '.'")
+    return errors
+
+def valid_contact(contact):
+    if contact[0] == "@":
+        if valid_instagram(contact):
+            return True
+    elif "@" in contact:
+        if valid_email(contact):
+            return True
+    else:
+        if valid_phone(contact):
+            return True
+    return False
+
 def valid_word(word):
     errors = []
     if word[0] not in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
@@ -22,7 +50,7 @@ def valid_word(word):
             errors.append("only letters")
     return errors
 
-def validate_event(host, event_name, event_type, description, address, event_date, time, price_range, phone):
+def validate_event(host, event_name, event_type, description, address, date, time, price_range, contact):
     res = ""
     if event_type == "select":
         res += "Event type" + "\n"
@@ -30,12 +58,13 @@ def validate_event(host, event_name, event_type, description, address, event_dat
         res += "Description"+ "\n"
     if not address or len(address) > 100:
         res += "Address" + "\n"
-    if not event_date or not (datetime.today().date() <= event_date <= (datetime.today().date() + timedelta(days=30))):
+    if not date or not (datetime.today().date() <= date <= (datetime.today().date() + timedelta(days=30))):
         res += "Date" + "\n"
     if not time:
         res += "Time" + "\n"
     if not price_range:
         res += "Price range" + "\n"
-    if valid_phone(phone):
-        res += "Phone number" + "\n"
-    return res + "not valid" if res != "" else ""
+    if not contact or not valid_contact(contact):
+        res += "Contact Details" + "\n"
+    
+    return res
