f8234b429c38cdf8e31a9051b8850b90dc801405
Ben Carmel <Bencarmel1@mail.tau.ac.il>
2025-01-25 22:34:20 +0200

    try 25

--- routes/api.py
+++ routes/api.py
@@ -5,13 +5,17 @@ api = Blueprint('api', __name__)
 
 @api.route('/events')   
 def get_events():
-    events = Event.query.order_by(Event.event_date.desc(), Event.time.desc()).all()
+    events = Event.query.order_by(Event.date.desc(), Event.time.desc()).all()
     return jsonify([{
+        "id": event.id,
+        "host": event.host,
+        "event_name": event.event_name,
         "event_type": event.event_type,
         "description": event.description,
         "address": event.address,
-        "event_date": event.event_date.isoformat(),
+        "date": event.date.isoformat(),
         "time": event.time.isoformat(),
         "price_range": event.price_range,
+        "contact": event.contact
     } for event in events])
 
