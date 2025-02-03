2c8973858fbaeb08badef1a5117dc93c077f7dbd
Ben Carmel <Bencarmel1@mail.tau.ac.il>
2025-02-03 18:57:08 +0200

    time4

--- tasks/add_auto.py
+++ tasks/add_auto.py
@@ -15,30 +15,40 @@ def get_next_saturday():
     return today + timedelta(days=days_ahead)
 
 def set_events():
-    event1 = Event(
-        host='Asaf Chetrit',
-        event_name='Carmel Market Ramen',
-        event_type='ramen',
-        description=' .אסף שטרית מכין ראמן צלול מציר עוף וטארה סויה, ותוספת של רולדת פרגית עם אצות. כל חמישי מ12-22 מחוץ לרומיה בר בשוק הכרמל',
-        address='Hacarmel 15, Tel Aviv',
-        date=get_next_thursday(),
-        time=datetime.strptime('12:00', "%H:%M").time(),
-        price_range='?? - ??')
-        
-    event2 = Event(
-        host='Asaf Chetrit',
-        event_name='Ramen at root',
-        event_type='ramen',
-        description='אסף שטרית מכין ראמן צלול מציר עוף וטארה סויה, ותוספת של רולדת פרגית עם אצות. כל מוצ"ש מ17 ברוטס בר.',
-        address='roots bar, Tel Aviv',
-        date=get_next_saturday(),
-        time=datetime.strptime('17:00', "%H:%M").time(),
-        price_range='?? - ??')
-    
     with app.app_context():
-        db.add(event1)
-        db.add(event2)
-        db.commit()
+        next_thursday = get_next_thursday()
+        next_saturday = get_next_saturday()
+        asaf_chetrit_events = Event.query.filter(Event.host == 'Asaf Chetrit').all()
+        flag1 = False
+        flag2 = False
+        for event in asaf_chetrit_events:
+            if event.date == next_thursday:
+                flag1 = True
+            if event.date == next_saturday:
+                flag2 = True
+        if not flag1:
+            event1 = Event(
+                host='Asaf Chetrit',
+                event_name='Carmel Market Ramen',
+                event_type='ramen',
+                description=' .אסף שטרית מכין ראמן צלול מציר עוף וטארה סויה, ותוספת של רולדת פרגית עם אצות. כל חמישי מ12-22 מחוץ לרומיה בר בשוק הכרמל',
+                address='Hacarmel 15, Tel Aviv',
+                date=get_next_thursday(),
+                time=datetime.strptime('12:00', "%H:%M").time(),
+                price_range='?? - ??')
+            db.session.add(event1)
+        if not flag2:
+            event2 = Event(
+                host='Asaf Chetrit',
+                event_name='Ramen at roots',
+                event_type='ramen',
+                description='אסף שטרית מכין ראמן צלול מציר עוף וטארה סויה, ותוספת של רולדת פרגית עם אצות. כל מוצ"ש מ17 ברוטס בר.',
+                address='roots bar, Tel Aviv',
+                date=get_next_saturday(),
+                time=datetime.strptime('17:00', "%H:%M").time(),
+                price_range='?? - ??')
+            db.session.add(event2)
+        db.session.commit()
 
 if __name__ == '__main__':
     set_events()
