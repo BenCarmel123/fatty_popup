c9b1dc71993055244396425ce9271bcd3f4eb90f
Ben Carmel <Bencarmel1@mail.tau.ac.il>
2025-02-12 00:39:54 +0200

    before sleep

--- form_validations/event_valid.py
+++ form_validations/event_valid.py
@@ -24,15 +24,15 @@ def validate_event(event_name, host_name, host_insta, chef1_name, chef1_insta, c
        res += "Event name" + "\n"
    if host_name and len(host_name) > 40:
        res += "Host name" + "\n"
-   if host_insta and len(host_insta) > 20:
+   if host_insta and len(host_insta) > 30:
        res += "Host Instagram" + "\n"
    if not chef1_name or (len(chef1_name) > 20 or len(valid_word(chef1_name))>0):
        res += "Chef 1 name" + "\n"
-   if not chef1_insta or (len(chef1_insta) > 20 or len(valid_instagram(chef1_insta))>0):
+   if not chef1_insta or (len(chef1_insta) > 30 or len(valid_instagram(chef1_insta))>0):
        res += "Chef 1 Instagram" + "\n"
    if chef2_name and len(chef2_name) > 40:
        res += "Chef 2 name" + "\n"
-   if chef2_insta and (len(chef2_insta) > 20 or len(valid_instagram(chef2_insta))>0):
+   if chef2_insta and (len(chef2_insta) > 30 or len(valid_instagram(chef2_insta))>0):
        res += "Chef 2 Instagram" + "\n"
    if type == "select":
        res += "Event type" + "\n"
