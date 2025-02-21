7dc3df7780cd3298901dc3f9012999b6f07fe280
Ben Carmel <Bencarmel1@mail.tau.ac.il>
2025-02-21 23:51:51 +0200

    add_auto update

--- tasks/add_auto.py
+++ tasks/add_auto.py
@@ -12,48 +12,9 @@ def get_next_weekday(target_day):
 
 def set_events():
     with app.app_context():
-        next_tuesday = get_next_weekday(1)
-        next_wednesday = get_next_weekday(2)
         next_thursday = get_next_weekday(3)
-        next_saturday = get_next_weekday(5)
-
-        fifi_events = {event.s_date for event in Event.query.filter(Event.chef1_name == 'Fifi').all()}
         asaf_events = {event.s_date for event in Event.query.filter(Event.chef1_name == 'Asaf Chetrit').all()}
-
         new_events = []
-
-        if next_tuesday not in fifi_events:
-            new_events.append(Event(
-                host_name='Yaya Levinsky',
-                host_insta='@yaya_levinsky',
-                chef1_name='Fifi',
-                chef1_insta='@fifisasianfood',
-                chef2_name='',
-                chef2_insta='',
-                event_name="Fifi's at Yaya",
-                type='ramen',
-                description='יפעת תבואה, הידועה בתור פיפיז ראמן, מגיעה עד הודעה חדשה ליאיא לוינסקי כל שלישי ורביעי מ19:00-23:00 להציע תפריט פיוזן של ראמן,כיסונים ועוד ביסים טובים',
-                location='Yaya Levinsky, Tel Aviv',
-                s_date=next_tuesday,
-                e_date=next_tuesday
-            ))
-
-        if next_wednesday not in fifi_events:
-            new_events.append(Event(
-                host_name='Yaya Levinsky',
-                host_insta='@yaya_levinsky',
-                chef1_name='Fifi',
-                chef1_insta='@fifisasianfood',
-                chef2_name='',
-                chef2_insta='',
-                event_name="Fifi's at Yaya",
-                type='ramen',
-                description='יפעת תבואה, הידועה בתור פיפיז ראמן, מגיעה עד הודעה חדשה ליאיא לוינסקי כל שלישי ורביעי מ19:00-23:00 להציע תפריט פיוזן של ראמן,כיסונים ועוד ביסים טובים',
-                location='Yaya Levinsky, Tel Aviv',
-                s_date=next_wednesday,
-                e_date=next_wednesday
-            ))
-
         if next_thursday not in asaf_events:
             new_events.append(Event(
                 host_name='',
@@ -68,22 +29,6 @@ def set_events():
                 location='Hacarmel 15, Tel Aviv',
                 s_date=next_thursday, 
                 e_date=next_thursday
-            ))
-
-        if next_saturday not in asaf_events:
-            new_events.append(Event(
-                host_name='roots Bar',
-                host_insta='@rootstlv',
-                chef1_name='Asaf Chetrit',
-                chef1_insta='@asafchetrit',
-                chef2_name='',
-                chef2_insta='',
-                event_name='Ramen at Roots',
-                type='ramen',
-                description='אסף שטרית מכין ראמן צלול מציר עוף וטארה סויה, ותוספת של רולדת פרגית עם אצות. כל מוצ"ש מ17 ברוטס בר.',
-                location='Roots Bar, Tel Aviv',
-                s_date=next_saturday,
-                e_date=next_saturday
             ))
 
         if new_events:
